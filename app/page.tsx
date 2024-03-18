0fac8f6e92804b5ea8d2b27c48713e10fe2203e1
M Gilang Januar <mgilangjanuar@gmail.com>
2024-03-18 10:24:37 +0700

    Add images and adjust styling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -189,9 +189,14 @@ export default function Home() {
               src: "./Lang Twitter Post.jpeg",
               width: 736,
               height: 1380,
+            },
+            {
+              src: "./Screenshot (15).png",
+              width: 1360,
+              height: 2026,
             }
           ]} />
-          <div className="flex justify-center pt-56 pb-36 bg-gradient-to-b from-base-100/0 via-base-100/100 to-base-100 -mt-72 z-40 absolute w-full">
+          <div className="flex justify-center pt-56 pb-36 bg-gradient-to-b from-base-100/0 via-base-100/100 to-base-100 -mt-64 z-40 absolute w-full">
             <div className="w-fit animate-gradient rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pb-0.5 pl-0.5">
               <input type="file" multiple={false} className="file-input w-full max-w-xs" onChange={e => setFiles(e.target.files)} accept="image/*,.heic" />
             </div>
