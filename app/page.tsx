47c0ea1c18a86863c955e688148e4f541aacf277
M Gilang Januar <mgilangjanuar@gmail.com>
2024-03-19 06:54:30 +0700

    Add CSS to hide scrollbars in different browsers

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-img-element */
 'use client'
 
 import * as ExifReader from 'exifreader'
@@ -17,6 +18,7 @@ export default function Home() {
   const [metadata, setMetadata] = useState<ExifReader.Tags | null>(null)
   const refWrap = useRef<HTMLDivElement>(null)
   const [loading, setLoading] = useState(false)
+  const [select, setSelect] = useState<{ src: string, width: number, height: number }>()
 
   useEffect(() => {
     if (files) {
@@ -51,6 +53,14 @@ export default function Home() {
     }
   // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isReady])
+
+  useEffect(() => {
+    if (select) {
+      console.log(select)
+      if (!window) return
+      ;(window as any)['app-preview'].showModal()
+    }
+  }, [select])
 
   return <div>
     <div className="min-h-svh relative">
@@ -87,7 +97,7 @@ export default function Home() {
           </div>
           <div className="container mx-auto pt-20 space-y-7">
             <div className="divider"></div>
-            <h2 className={`${caveat.className} text-3xl text-center`}>
+            <h2 className={`${caveat.className} text-3xl md:text-4xl text-center`}>
               Thank you for using our service!
             </h2>
             <div className="flex justify-center items-center gap-3 flex-wrap">
@@ -115,16 +125,16 @@ export default function Home() {
 
         {blob && metadata ? <div className="overflow-auto w-full">
           <div className="border hidden w-fit mx-auto" ref={refWrap}>
-            <div id="main" className="p-10 flex-col items-center space-y-20 mx-auto bg-white">
+            <div id="main" className="p-9 flex-col items-center space-y-16 mx-auto bg-white">
               <div>
                 <img src={URL.createObjectURL(blob as Blob)} onLoad={() => setIsReady(true)} className="max-w-7xl" alt="Image" />
               </div>
-              <div className="text-center text-gray-400 pb-16">
+              <div className="text-center text-gray-400 pb-7">
                 {metadata?.Model?.description || metadata?.['Device Manufacturer']?.description ? <p className="text-4xl">
-                  Shot on <strong className="font-semibold text-black">{metadata?.Model?.description || metadata?.['Device Manufacturer']?.description}</strong>
+                  Shot on <strong className="font-bold text-black">{metadata?.Model?.description || metadata?.['Device Manufacturer']?.description}</strong> {metadata?.Model?.description && metadata?.['Make']?.description && metadata?.Model?.description !== metadata?.['Make']?.description ? <strong className="font-medium text-black">{metadata?.['Make']?.description}</strong> : <></>}
                 </p> : <></>}
-                <p className="text-3xl mt-5 space-x-4">
-                  {metadata?.FocalLengthIn35mmFilm?.description && metadata?.FocalLengthIn35mmFilm?.description.toLowerCase() !== 'unknown' ? <span>{metadata?.FocalLengthIn35mmFilm?.description}mm</span> : <></>}
+                <p className="text-2xl mt-4 space-x-3">
+                  {metadata?.FocalLength?.description && metadata?.FocalLength?.description.toLowerCase() !== 'unknown' ? <span>{metadata?.FocalLength?.description.split(' ')[0]}mm</span> : <></>}
                   {metadata?.FNumber?.description ? <span>{metadata?.FNumber?.description}</span> : <></>}
                   {metadata?.ExposureTime?.description ? <span>{metadata?.ExposureTime?.description}s</span> : <></>}
                   {metadata?.ISOSpeedRatings?.description ? <span>ISO{metadata?.ISOSpeedRatings?.description}</span> : <></>}
@@ -134,107 +144,112 @@ export default function Home() {
           </div>
         </div> : <div className="relative box">
           {/* <img src="./Screenshot (1).png" className="max-w-md w-full border mx-auto" alt="Image" /> */}
-          <div className="flex justify-center py-64 -mt-60 bg-gradient-to-t from-base-100/0 via-base-100/100 to-base-100/100 absolute w-full z-10">
+          <div className="flex justify-center py-20 -mt-16 bg-gradient-to-t from-base-100/0 via-base-100/100 to-base-100/100 absolute w-full z-10">
             <div className="w-fit animate-gradient rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pb-1 pr-1">
               <input type="file" multiple={false} className="file-input md:file-input-lg w-full max-w-xs md:max-w-sm" onChange={e => setFiles(e.target.files)} accept="image/*,.heic" />
             </div>
           </div>
           <div className="pt-10">
-            <PhotoAlbum layout="columns" photos={[
+            <PhotoAlbum layout="columns" onClick={({ photo }) => setSelect(photo)} photos={[
               {
-                src: "./Screenshot (1).png",
+                src: "/Screenshot (1).png",
                 width: 1472,
                 height: 2152,
               },
               {
-                src: "./Download (3).png",
+                src: "/Download (3).png",
                 width: 1472,
                 height: 2748,
               },
               {
-                src: "./Basboi fan.png",
+                src: "/Basboi fan.png",
                 width: 736,
                 height: 688,
               },
               {
-                src: "./Gunturr Prewed.jpeg",
+                src: "/Gunturr Prewed.jpeg",
                 width: 736,
                 height: 628,
               },
               {
-                src: "./Lang on X (1).jpeg",
+                src: "/Lang on X (1).jpeg",
                 width: 1472,
                 height: 2152,
               },
               {
-                src: "./Lang on X (2).jpeg",
+                src: "/Lang on X (2).jpeg",
                 width: 879,
                 height: 1296,
               },
               {
-                src: "./Lang on X.jpeg",
+                src: "/Lang on X.jpeg",
                 width: 736,
                 height: 1374,
               },
               {
-                src: "./Lang tweet photo.jpeg",
+                src: "/Lang tweet photo.jpeg",
                 width: 736,
                 height: 558,
               },
               {
-                src: "./Lang.jpeg",
+                src: "/Lang.jpeg",
                 width: 736,
                 height: 628,
               },
               {
-                src: "./Screenshot (14).png",
+                src: "/Screenshot (14).png",
                 width: 1360,
                 height: 1040,
               },
               {
-                src: "./Screenshot (11).png",
+                src: "/Screenshot (11).png",
                 width: 1360,
                 height: 1173,
               },
               {
-                src: "./Lang Twitter Post.jpeg",
+                src: "/Lang Twitter Post.jpeg",
                 width: 736,
                 height: 1380,
               },
               {
-                src: "./Screenshot (16).png",
+                src: "/Screenshot (16).png",
                 width: 1360,
                 height: 1173,
               },
               {
-                src: "./Screenshot (15).png",
+                src: "/Screenshot (15).png",
                 width: 1360,
                 height: 2026,
               },
               {
-                src: "./Urayz tweet.jpeg",
+                src: "/Urayz tweet.jpeg",
                 width: 1360,
                 height: 2026,
               },
               {
-                src: "./GI-e1fCboAAYKrk.jpeg",
+                src: "/GI-e1fCboAAYKrk.jpeg",
                 width: 1073,
                 height: 2048,
               },
               {
-                src: "./GI-e1h4bcAAScYi.jpeg",
+                src: "/GI-e1h4bcAAScYi.jpeg",
                 width: 1360,
                 height: 1173,
               },
               {
-                src: "./GI-e2G9awAA1Wjo.jpeg",
+                src: "/GI-e2G9awAA1Wjo.jpeg",
                 width: 1073,
                 height: 2048,
               },
               {
-                src: "./What the Frame.png",
+                src: "/What the Frame.png",
                 width: 1360,
                 height: 1280,
+              },
+              {
+                src: "/What the Frame (1).png",
+                width: 1272,
+                height: 1052,
               },
             ]} />
           </div>
@@ -246,18 +261,18 @@ export default function Home() {
           <form method="dialog">
             <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
           </form>
-          <h3 className="font-bold text-lg">Edit</h3>
+          <h3 className={`font-bold text-2xl ${caveat.className}`}>Edit</h3>
           <form className="space-y-3 py-3" onSubmit={async e => {
             e.preventDefault()
             const data = Object.fromEntries(new FormData(e.target as HTMLFormElement))
-            const metadata = {
+            setMetadata(metadata => ({
+              ...metadata,
               Model: { description: data.device },
-              FocalLengthIn35mmFilm: { description: data.focal },
+              FocalLength: { description: data.focal },
               FNumber: { description: data.fnumber },
               ExposureTime: { description: data.exposure },
               ISOSpeedRatings: { description: data.iso },
-            }
-            setMetadata(metadata as ExifReader.Tags)
+             } as ExifReader.Tags))
 
             setLoading(true)
 
@@ -285,7 +300,7 @@ export default function Home() {
               <label className="label">
                 <span className="label-text">Focal Length</span>
               </label>
-              <input type="text" name="focal" className="input input-bordered" defaultValue={metadata?.FocalLengthIn35mmFilm?.description} />
+              <input type="text" name="focal" className="input input-bordered" defaultValue={metadata?.FocalLength?.description} />
             </div>
             <div className="form-control">
               <label className="label">
@@ -320,11 +335,10 @@ export default function Home() {
 
       <dialog id="app-loading" className="modal modal-bottom sm:modal-middle z-10">
         <div className="modal-box">
-          <h3 className="font-bold text-lg flex items-center gap-5 justify-between">
+          <h3 className={`font-bold text-2xl ${caveat.className}`}>
             Processing...
-            <span className="loading loading-dots"></span>
           </h3>
-          <p>
+          <p className="py-3">
             Please wait while we process your image.
           </p>
         </div>
@@ -332,6 +346,22 @@ export default function Home() {
           {/* <button>close</button> */}
         </form>
       </dialog>
+
+      <dialog id="app-preview" className="modal modal-bottom sm:modal-middle z-10">
+        <div className="modal-box no-scrollbar">
+          <form method="dialog">
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+          </form>
+          <h3 className={`font-bold text-2xl ${caveat.className}`}>Preview</h3>
+          <div className="py-3">
+            {select?.src ? <img src={select?.src as string} width={select?.width} height={select?.height} alt="Preview" className="w-full" /> : <></>}
+          </div>
+        </div>
+        <form method="dialog" className="modal-backdrop">
+          <button>close</button>
+        </form>
+      </dialog>
+
     </div>
     <div className="flex justify-center items-center py-8 flex-col mt-0 gap-2">
       <div className="divider"></div>
