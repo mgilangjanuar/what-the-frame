7c3465e200ad6dec2cbba5f8be7001aa97aa5f20
M Gilang Januar <mgilangjanuar@gmail.com>
2024-03-18 20:20:06 +0700

    Update file input styling in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -135,7 +135,7 @@ export default function Home() {
         </div> : <div className="relative box">
           {/* <img src="./Screenshot (1).png" className="max-w-md w-full border mx-auto" alt="Image" /> */}
           <div className="flex justify-center py-64 -mt-60 bg-gradient-to-t from-base-100/0 via-base-100/100 to-base-100/100 absolute w-full z-10">
-            <div className="w-fit animate-gradient rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pb-0.5 pr-0.5">
+            <div className="w-fit animate-gradient rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pb-1 pr-1">
               <input type="file" multiple={false} className="file-input md:file-input-lg w-full max-w-xs md:max-w-sm" onChange={e => setFiles(e.target.files)} accept="image/*,.heic" />
             </div>
           </div>
