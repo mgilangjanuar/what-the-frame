3b082a440d9821226c1bc1ce9aca74b169a5924e
M Gilang Januar <mgilangjanuar@gmail.com>
2024-03-18 13:36:55 +0700

    Update page layout and styling

--- app/page.tsx
+++ app/page.tsx
@@ -201,7 +201,7 @@ export default function Home() {
               height: 2026,
             },
           ]} />
-          <div className="flex justify-center py-44 bg-gradient-to-b from-base-100/0 via-base-100/100 to-base-100 -mt-64 z-40 absolute w-full">
+          <div className="flex justify-center py-52 bg-gradient-to-b from-base-100/0 via-base-100/100 to-base-100 -mt-64 z-40 absolute w-full">
             <div className="w-fit animate-gradient rounded-lg bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500 pb-0.5 pl-0.5">
               <input type="file" multiple={false} className="file-input md:file-input-lg w-full max-w-xs md:max-w-sm" onChange={e => setFiles(e.target.files)} accept="image/*,.heic" />
             </div>
@@ -301,7 +301,7 @@ export default function Home() {
         </form>
       </dialog>
     </div>
-    <div className="flex justify-center items-center py-8 flex-col mt-20 gap-2">
+    <div className="flex justify-center items-center py-8 flex-col mt-36 gap-2">
       <div className="divider"></div>
       <div className="container mx-auto">
         <div className="flex gap-3 justify-center md:justify-between items-center w-full flex-wrap">
